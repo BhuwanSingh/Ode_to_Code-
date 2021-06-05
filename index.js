a9a423d45407a7c7c68e9f89075586c653cdf3ba
Bhuwan Singh <bhuwankumar2000@gmail.com>
2021-06-05 13:41:50 +0530

    API iter

--- index.js
+++ index.js
@@ -2,11 +2,27 @@ const express =  require("express");
 const app = express();
 const fs = require("fs");
 
-app.get( '/json' , function(req, res , next) {
-    console.log("working");
-    res.json({"one" : "two"});
+var ans = fs.readFileSync('output.json');
+let fans = JSON.parse(ans);
+// console.log(fans);
+
+app.use( express.urlencoded({
+    extended : true
+}))
+
+app.use(express.json())
+
+app.post('/api' , (req, res) => {
+    for(const a of req.body ){
+        console.log(a);
+    }
+})
+
+app.get( '/api' , function(req, res , next) {
+    console.log("working-get");
+    res.json({fans});
 })
 
 app.listen(3000 , () =>{
-    console.log("working");
+    console.log("listining");
 })
