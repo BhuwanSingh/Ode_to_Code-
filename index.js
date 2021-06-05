1653ed48ce119cd9bc4afb79c581213c09284cfc
Bhuwan Singh <bhuwankumar2000@gmail.com>
2021-06-05 15:42:10 +0530

    API iteration - 1

--- index.js
+++ index.js
@@ -6,35 +6,22 @@ app.use(express.json());
 
 app.get("/api", (req, res) => {
     console.log("here");
-    const { a } = req.body;
-    console.log(a);
-    console.log(a['audio'])
-    // fs.writeFileSync("input.txt", JSON.stringify(a), err => {
-    //     if (err) {
-    //         console.error(err);
-    //         return;
-    //     }
-    // });
-    // app.get("/api", (req, res) => {
-        var ans = fs.readFileSync("output.json");
-        let fans = JSON.parse(ans);
-        console.log("working-get");
-        res.json({ fans });
-    // })
+    const a = req.body;
+    console.log(JSON.stringify(a));
+
+    var ans = fs.readFileSync("output.json");
+    let fans = JSON.parse(ans);
+    console.log("working-get");
+    res.json({ fans });
+    fs.writeFileSync("input.txt", JSON.stringify(a), err => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+    });
+
 });
 
 app.listen(3001, () => {
     console.log("listining");
 });
-
-// process.on('exit' , () => {
-//     console.log("exiting1");
-// })
-
-// process.on('uncaughtException', () => {
-//     console.log("exiting2");
-// })
-
-// process.on('SIGTERM', () => {
-//     console.log("exiting3");
-// })
