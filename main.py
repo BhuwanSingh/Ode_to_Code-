7fe20b989d66c21c6a15c4f633480f130d20467e
Bhuwan Singh <bhuwankumar2000@gmail.com>
2021-06-06 11:15:04 +0530

    final

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import librosa
 import json
 import base64
 import soundfile as sf
+from datetime import datetime
+import re
 
 import time
 start_time = time.time()
@@ -16,6 +18,7 @@ else:
     exit()
 
 audio = input_data_dict["audio"]
+question = input_data_dict['question_key']
 wav_file = open("temp.wav", "wb")
 decode_string = base64.b64decode(audio)
 wav_file.write(decode_string)
@@ -42,20 +45,65 @@ with sr.AudioFile(filename) as source:
 # print(to_match)
 
 b = []
+if question == 'q1':
+    for i in to_match:
+        if i in a:
+            b.append(i)
+    k = { "answers" : b}
+    f = open("output.json", "w+")
+    # arr = a.split()
+    f.write(json.dumps(k))
+    f.close()
 
-for i in to_match:
-    if i in a:
-        b.append(i)
+
+# if question == 'q2':
+#     num = int(audio.split("lakh")[0])
+
+# limits = []
+
+# limits.append(int(options[0].split("<")[1].split("lakh")[0]))
+
+
+# for i in range(1,len(options)-1):
+#     num = int(audio.split("lakh")[0])
+
+#     limits = []
+
+#     limits.append(int(options[0].split("<")[1].split("lakh")[0]))
+
+
+#     for i in range(1,len(options)-1):
+#         check = options[i].split("-")
+#         upper = int(check[1].split("lakh")[0])
+#         limits.append(int(upper))
+
+#     pos=-1
+
+#     for i in range(0,len(options)-1):
+#         if num <= limits[i]:
+#             pos=i
+#             break
+
+# if pos is -1:
+#     print( options[len(options)-1])
+# else:
+#     print(options[pos])
+
+
+if question == 'q3':
+    listToStr = ' '.join(map(str, a))
+    date_time_obj = datetime.strptime(listToStr, '%d/%m/%y %H:%M:%S')
+    # b.append[date_time_obj]
+    k = { "answers" : date_time_obj}
+    f = open("output.json", "w+")
+    # arr = a.split()
+    f.write(json.dumps(k))
+    f.close()
+
 
 # print(b)
 
-k = { "answers" : b}
-print(k)
 
-f = open("output.json", "w+")
-# arr = a.split()
-f.write(json.dumps(k))
-f.close()
 
 print(a)
 print("--- %s seconds ---" % (time.time() - start_time))
